e87cb81c15fea790448b9718e8f39bf5f8456512
Matt Lyon <matthewlyon18@gmail.com>
2025-09-14 19:04:20 +0100

    fixed eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,34 +3,60 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import eslintImport from 'eslint-plugin-import';
+import eslintImportLineSorter from 'eslint-plugin-import-line-sorter';
 
-export default tseslint.config([
-    globalIgnores(['dist']),
+export default tseslint.config(
+    {
+        ignores: ['dist'],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
     {
         files: ['**/*.{ts,tsx,js,jsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs['recommended-latest'],
-            reactRefresh.configs.vite,
-            'plugin:prettier/recommended',
-        ],
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            import: eslintImport,
+            'import-line-sorter': eslintImportLineSorter,
+        },
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
         rules: {
-            'prettier/prettier': [
-                'error',
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'import/no-duplicates': 'off',
+            'import/no-unresolved': 'off',
+            'import/order': [
+                'warn',
                 {
-                    singleQuote: true,
-                    jsxSingleQuote: true,
-                    printWidth: 80,
-                    tabWidth: 4,
-                    trailingComma: 'es5',
+                    pathGroups: [
+                        {
+                            pattern: '@recipe/**',
+                            group: 'external',
+                            position: 'after',
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ['@recipe/**'],
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        ['sibling', 'parent'],
+                        'index',
+                        'unknown',
+                    ],
+                    'newlines-between': 'always',
                 },
             ],
+            'import-line-sorter/sort-imports': 'warn',
+            'import-line-sorter/no-multiline-imports': 'warn',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
         },
-    },
-]);
+    }
+);
